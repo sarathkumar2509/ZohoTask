e022f1f873ab51d9531358f821b211ea3f8865cc
sarathkumar-optisol <79133752+sarathkumar-optisol@users.noreply.github.com>
2021-07-17 16:42:29 +0530

    Phase 1 Simple RecyclerView Setup

---
+++ app/src/main/java/com/droid/zohotask/adapter/UserListAdapter.kt
@@ -0,0 +1,69 @@
+package com.droid.zohotask.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.droid.zohotask.R
+import com.droid.zohotask.databinding.FragmentUserListBinding
+import com.droid.zohotask.databinding.UserlistItemBinding
+import com.droid.zohotask.listener.OnUserListItemClick
+import com.droid.zohotask.model.response.Result
+
+/**
+ * Created by SARATH on 17-07-2021
+ */
+class UserListAdapter(private val context: Context, private val onUserListItemClickListener: OnUserListItemClick) : RecyclerView.Adapter<UserListAdapter.UserListViewHolder>(){
+
+    inner class UserListViewHolder(val binding : UserlistItemBinding) : RecyclerView.ViewHolder(binding.root)
+
+    private val diffCallback = object : DiffUtil.ItemCallback<Result>(){
+        override fun areItemsTheSame(oldItem: Result, newItem: Result): Boolean {
+            return oldItem.email == newItem.email
+        }
+
+        override fun areContentsTheSame(oldItem: Result, newItem: Result): Boolean {
+            return oldItem.email == newItem.email
+        }
+
+    }
+
+
+    private val differ = AsyncListDiffer(this,diffCallback)
+    var userListResponse : MutableList<Result>
+
+        get()=differ.currentList
+        set(value) {differ.submitList(value)}
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): UserListAdapter.UserListViewHolder {
+        return UserListViewHolder(UserlistItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        ))
+    }
+
+    override fun onBindViewHolder(holder: UserListAdapter.UserListViewHolder, position: Int) {
+        val userListResponse = userListResponse[position]
+        holder.binding.apply {
+
+            Glide.with(context).load(userListResponse.picture.medium).placeholder(R.drawable.ic_baseline_account_circle_24).circleCrop().into(ivPicture)
+
+            userListResponse.picture.thumbnail
+        }
+        holder.itemView.setOnClickListener{
+            onUserListItemClickListener.onClick(userListResponse)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+}
--- app/src/main/java/com/droid/zohotask/fragment/UserListFragment.kt
+++ app/src/main/java/com/droid/zohotask/fragment/UserListFragment.kt
@@ -3,13 +3,18 @@ package com.droid.zohotask.fragment
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.droid.zohotask.R
+import com.droid.zohotask.adapter.UserListAdapter
 import com.droid.zohotask.databinding.FragmentUserListBinding
+import com.droid.zohotask.listener.OnUserListItemClick
 import com.droid.zohotask.main.MainViewModel
+import com.droid.zohotask.model.response.Result
 import kotlinx.coroutines.flow.collect
 
 /**
@@ -20,6 +25,8 @@ class UserListFragment : Fragment(R.layout.fragment_user_list){
     private lateinit var binding : FragmentUserListBinding
 
     private lateinit var viewModel: MainViewModel
+
+    private lateinit var userListAdapter: UserListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,12 +43,15 @@ class UserListFragment : Fragment(R.layout.fragment_user_list){
 
         viewModel.getUserList()
 
+        setupRecyclerView()
+
         lifecycleScope.launchWhenCreated {
             viewModel.userList.collect { event ->
                 when(event){
                     is MainViewModel.UserListEvent.Success ->{
                         binding.pbUserListFragment.isVisible = false
                         var result = event.result
+                        userListAdapter.userListResponse = event.result.toMutableList()
                         Log.d("UserListFragment","$result")
                     }
                     is MainViewModel.UserListEvent.Failure->{
@@ -54,5 +64,18 @@ class UserListFragment : Fragment(R.layout.fragment_user_list){
                 }
             }
         }
+    }
+
+    private fun setupRecyclerView() {
+            binding.rvUserList.apply {
+                userListAdapter = UserListAdapter(context,object : OnUserListItemClick{
+                    override fun onClick(result: Result) {
+                        Toast.makeText(context,"Clicked",Toast.LENGTH_SHORT).show()
+                    }
+                })
+
+                adapter = userListAdapter
+                layoutManager =LinearLayoutManager(context)
+            }
     }
 }
